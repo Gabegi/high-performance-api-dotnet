97a5371dab3bf781b85ab397039ad292ebb2c091
Gabegi <gabriel_pirastru@hotmail.fr>
2025-10-02 18:57:36 +0530

    dbcontext

--- ApexShop.Domain/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using ApexShop.Domain.Entities;
-
-namespace ApexShop.Domain.Interfaces;
-
-public interface IUserRepository : IRepository<User>
-{
-    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-    Task<IEnumerable<User>> GetActiveUsersAsync(CancellationToken cancellationToken = default);
-}
