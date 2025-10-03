d2a74fabdddef9101bd42f4b7943d98d6abed3ac
Gabegi <gabriel_pirastru@hotmail.fr>
2025-10-03 14:04:04 +0530

    benchmarks

--- ApexShop.Benchmarks/Program.cs
+++ ApexShop.Benchmarks/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ApexShop.Benchmarks.Micro;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Running;
+
+var config = DefaultConfig.Instance
+    .AddExporter(CsvExporter.Default)
+    .AddExporter(HtmlExporter.Default);
+
+BenchmarkRunner.Run<DatabaseBenchmarks>(config);
