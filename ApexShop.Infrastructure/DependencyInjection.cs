17b384358e811677adde553d0acf421eff8c53ae
Gabegi <gabriel_pirastru@hotmail.fr>
2025-10-07 15:42:34 +0530

    efcore optimisation

--- ApexShop.Infrastructure/DependencyInjection.cs
+++ ApexShop.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,5 @@
 using ApexShop.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
